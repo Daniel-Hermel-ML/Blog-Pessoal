82d8127116eb783608ac6f6e5ec1ce05f9ac0dc2
Daniel-Hermel-MELI <daniel.hermel@mercadolivre.com>
2023-02-01 16:45:12 -0300

    Envio do exercício Blog Pessoal - do 12 e 13 - correção item 5.2

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -58,10 +58,13 @@ public class PostagemController {
     //Método Put:
     @PutMapping
     public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
-        return postagemRepository.findById(postagem.getId())
-                .map(resposta -> ResponseEntity.status(HttpStatus.OK)
-                .body(postagemRepository.save(postagem)))
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        if (postagemRepository.existsById(postagem.getId())){
+
+            if (temaRepository.existsById(postagem.getTema().getId()))
+                return ResponseEntity.status(HttpStatus.OK)
+                .body(postagemRepository.save(postagem));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
